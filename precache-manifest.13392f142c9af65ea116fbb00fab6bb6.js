b2c156c2f58792a809914d9a8b28aab9251772d4
karanjariwala <karanjariwala@gmail.com>
2019-04-14 15:02:05 +0530

    Updates

---
+++ precache-manifest.13392f142c9af65ea116fbb00fab6bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "482d75ce8015d97648c85b59713484a4",
+    "url": "/myprofile/static/media/DP.482d75ce.jpg"
+  },
+  {
+    "revision": "3fec3ff729f1e05922eb",
+    "url": "/myprofile/static/js/runtime~main.fcf54a66.js"
+  },
+  {
+    "revision": "ba5bde850dbb0d9a56b5",
+    "url": "/myprofile/static/js/main.46dea6db.chunk.js"
+  },
+  {
+    "revision": "ec9daebf6d6606328f4c",
+    "url": "/myprofile/static/js/2.e3893474.chunk.js"
+  },
+  {
+    "revision": "ba5bde850dbb0d9a56b5",
+    "url": "/myprofile/static/css/main.58b15db0.chunk.css"
+  },
+  {
+    "revision": "a249ed8c77b762f8ff62876a7fa0cc62",
+    "url": "/myprofile/index.html"
+  }
+];
